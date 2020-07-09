192610472c5b728897a56259292b3abc1ecbe3cd
huyfr <61008601+huyfr@users.noreply.github.com>
2020-07-09 10:26:46 +0700

    deploy blog component

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {IPost} from '../IPost';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {PostService} from '../post.service';
 
 @Component({
   selector: 'app-blog',
@@ -7,9 +10,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogComponent implements OnInit {
 
-  constructor() { }
+  postList: IPost[] = [];
+  postForm: FormGroup;
+  submitted = false;
 
-  ngOnInit(): void {
+  constructor(private postService: PostService, private formBuilder: FormBuilder) {
   }
 
+  ngOnInit(): void {
+    this.postForm = this.formBuilder.group({
+      title: ['', [Validators.required, Validators.minLength(10)]],
+      body: ['', [Validators.required, Validators.minLength(10)]],
+    });
+    this.postService.getPost().subscribe(next => (this.postList = next), error => (this.postList = []));
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.postForm.valid) {
+      const {value} = this.postForm;
+      this.postService.createPost(value).subscribe(next => {
+        this.postList.unshift(next);
+        this.postForm.reset({
+          title: '',
+          body: ''
+        });
+      }, error => console.log(error));
+    }
+  }
+
+  deletPost(id: number) {
+    const currentPost = this.postList[id];
+    this.postService.deletePost(currentPost.id).subscribe(() => {
+      this.postList = this.postList.filter(post => post.id !== currentPost.id);
+    });
+  }
+
+  get rfc() {
+    return this.postForm.controls;
+  }
 }
