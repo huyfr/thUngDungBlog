afdcd635d1bd2d3d9deeca8c376e9ff694864bd7
huyfr <61008601+huyfr@users.noreply.github.com>
2020-07-09 10:26:09 +0700

    additional modules support blog.component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BlogComponent } from './blog/blog.component';
-import { BlogEditComponent } from './blog-edit/blog-edit.component';
-import { BlogDetailComponent } from './blog-detail/blog-detail.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {BlogComponent} from './blog/blog.component';
+import {BlogEditComponent} from './blog-edit/blog-edit.component';
+import {BlogDetailComponent} from './blog-detail/blog-detail.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {PostService} from './post.service';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -19,8 +22,11 @@ import { BlogDetailComponent } from './blog-detail/blog-detail.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [PostService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
