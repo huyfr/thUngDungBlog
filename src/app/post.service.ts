d0de64eb7072b3de6ae05c8650568706d225e917
huyfr <61008601+huyfr@users.noreply.github.com>
2020-07-09 10:23:33 +0700

    deploy post.service.ts

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,9 +1,37 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {IPost} from './IPost';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
+  private readonly API_URL = 'http://jsonplaceholder.typicode.com/posts';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  getPost(count = 10): Observable<IPost[]> {
+    return this.http.get<IPost[]>(this.API_URL).pipe(
+      map(respone => respone.filter((post, i) => i < count))
+    );
+  }
+
+  getPostById(id: number): Observable<IPost> {
+    return this.http.get<IPost>(`${this.API_URL}/${id}`);
+  }
+
+  createPost(post: Partial<IPost>): Observable<IPost> {
+    return this.http.post<IPost>(this.API_URL, post);
+  }
+
+  deletePost(id: number): Observable<IPost> {
+    return this.http.delete<IPost>(`${this.API_URL}/${id}`);
+  }
+
+  updatePost(post: IPost): Observable<IPost> {
+    return this.http.patch<IPost>(`${this.API_URL}/${post.id}`, post);
+  }
 }
